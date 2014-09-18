df3fae65023700b0607b2ab63287de0ce43c1708
dkanderson <dkanderson.designs@gmail.com>
2014-09-18 02:59:55 -0400

    rearranged codestructure, implement pub/sub model, jsLint, abstract header view...

--- js/script.js
+++ js/script.js
@@ -1,29 +1,81 @@
-( function () {
+(function () {
 
-     'use strict';
+    'use strict';
 
+    window.vent = _.extend({}, Backbone.Events);
 
-
-     var App = {    
+    var App = {
         Models: {},
         Views: {},
         Collections: {},
         Routes: {},
         Helpers: {}
-     };
+    };
 
-     App.Models.Story = Backbone.Model.extend({
-     });
+    App.Models.Story = Backbone.Model.extend({
+    });
 
-     App.Collections.Stories = Backbone.Collection.extend({
+    App.Collections.Stories = Backbone.Collection.extend({
         model: App.Models.Story,
         url: './data/news.json',
         parse: function (resp) {
             return resp.posts;
         }
-     });
+    });
 
-     App.Views.Story = Backbone.View.extend({
+    App.Views.App = Backbone.View.extend({
+
+        template: Handlebars.compile($('#storiesTpl').html()),
+
+        initialize: function () {
+            window.vent.on('collection:loaded', this.render, this);
+        },
+
+        render: function (theCollection) {
+            this.$el.html(this.template);
+
+            this.stories = new App.Views.Stories({collection: theCollection});
+            this.featured = new App.Views.Featured({collection: theCollection});
+
+            this.$el.append(this.featured.$el);
+            this.$el.append(this.dividerBar());
+            this.$el.append(this.stories.$el);
+
+            return this;
+        },
+
+        dividerBar: function () {
+            return '<h3 id="featured-tb">Featured Stories</h3>';
+
+        }
+    });
+
+    App.Views.HeaderView = Backbone.View.extend({
+        tagName: 'header',
+
+        className: 'main-header',
+
+        template: Handlebars.compile($('#headerTpl').html()),
+
+        events: {
+            'click .mobile-trigger': 'openPanel',
+        },
+
+        initialize: function () {
+            this.render();
+        },
+
+        render: function () {
+            this.$el.html(this.template);
+        },
+
+        openPanel: function (e) {
+            e.preventDefault();
+            $('#right-panel').toggleClass('show-panel');
+        }
+    });
+
+    App.Views.Story = Backbone.View.extend({
         tagName: 'li',
 
         template: Handlebars.compile($('#storyTpl').html()),
@@ -36,62 +88,65 @@
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         }
-     });
+    });
 
-     App.Views.Stories = Backbone.View.extend({
-         el: '#list-content',
+    App.Views.Stories = Backbone.View.extend({
+        el: '#list-content',
 
-         initialize: function () {
+        initialize: function () {
             this.render();
             this.prettyDate();
-         },
+        },
 
-         render:function () {
+        render: function () {
             this.collection.each(this.populate, this);
             return this;
-         },
+        },
 
-         populate: function (stories) {
+        populate: function (stories) {
             var story = new App.Views.Story({model: stories});
             this.$el.append(story.render().el);
-         },
+        },
 
-         prettyDate: function () {
+        prettyDate: function () {
             // Takes an ISO time and returns a string representing how
             // long ago the date represents.
-            function prettyDate (time) {
-                var date = new Date((time || "").replace(/-/g,"/")),
+            function prettyDate(time) {
+                var date = new Date((time || "").replace(/-/g, "/")),
                     diff = (((new Date()).getTime() - date.getTime()) / 1000),
                     day_diff = Math.floor(diff / 86400);
 
-                if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 365 )
+                if (isNaN(day_diff) || day_diff < 0 || day_diff >= 365) {
                     return;
+                }
 
-               return day_diff == 0 && ( diff < 60 && "just now" ||
-                        diff < 120 && "1 minute ago" ||
-                        diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-                        diff < 7200 && "1 hour ago" ||
-                        diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-                    day_diff == 1 && "Yesterday" ||
-                    day_diff < 7 && day_diff + " days ago" ||
-                    day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago" ||
-                    day_diff < 365 && Math.ceil( day_diff / 12 ) + " months ago";
+                return ((day_diff === 0 && diff < 60 && "just now") ||
+                        (diff < 120 && "1 minute ago") ||
+                        (diff < 3600 && (Math.floor(diff / 60) + " minutes ago")) ||
+                        (diff < 7200 && "1 hour ago") ||
+                        (diff < 86400 && Math.floor(diff / 3600) + " hours ago")) ||
+                    (day_diff === 1 && "Yesterday") ||
+                    (day_diff < 7 && day_diff + " days ago") ||
+                    (day_diff < 31 && Math.ceil(day_diff / 7) + " weeks ago") ||
+                    (day_diff < 365 && Math.ceil(day_diff / 12) + " months ago");
             }
 
-            function prettyLinks () {
-                var links = document.getElementsByClassName('date');
-                for ( var i = 0; i < links.length; i++ )
-                    if ( links[i].title ) {
-                        var pdate = prettyDate(links[i].title);
+            function prettyLinks() {
+                var i, pdate, links = document.getElementsByClassName('date');
+                for (i = 0; i < links.length; i += 1) {
+                    if (links[i].title) {
+                        pdate = prettyDate(links[i].title);
 
-                        if ( pdate )
+                        if (pdate) {
                             links[i].innerHTML = pdate;
+                        }
                     }
+                }
             }
             prettyLinks();
             setInterval(prettyLinks, 5000);
 
-         }
+        }
     });
 
     App.Views.NavPanel = Backbone.View.extend({
@@ -109,31 +164,6 @@
         }
     });
 
-    App.Views.App = Backbone.View.extend({
-        el: '#appMain',
-
-        template: Handlebars.compile($('#appTpl').html()),
-
-        initialize: function () {
-            this.render();
-        },
-
-        events: {
-            'click .mobile-trigger': 'openPanel',
-        },
-
-
-        render: function () {
-            this.$el.html(this.template());
-            return this;
-        },
-
-        openPanel: function (e) {
-            e.preventDefault();
-            $('#right-panel').toggleClass('show-panel');
-        }
-    });
-
     App.Views.Featured = Backbone.View.extend({
         el: '#featured',
 
@@ -148,27 +178,36 @@
             return this;
         }
     });
-     
+
     App.Routes.Kickstart = Backbone.Router.extend({
         routes: {
-            '': 'start'
+            '': 'start',
+            '/:id': 'singlePage'
         },
-        start: function () {
-            var app = new App.Views.App();
-            var news = new App.Collections.Stories();
-                 news.fetch({
-                    success: function (theCollection) {
-                        var s = new App.Views.Stories({collection: theCollection}),
-                            np = new App.Views.NavPanel(),
-                            feature = new App.Views.Featured({collection: theCollection});
-                 }
-             });
-        }
-     })
-     
-     
 
-var x = new App.Routes.Kickstart();
-Backbone.history.start();
+        initialize: function () {
+            this.headerView = new App.Views.HeaderView();
+            this.navPanel = new App.Views.NavPanel();
+            $('.header').html(this.headerView.el);
+        },
+
+        start: function () {
+            this.app = new App.Views.App();
+            $('#appMain').html(this.app.el);
+            this.news = new App.Collections.Stories();
+            this.news.fetch({
+                success: function (theCollection) {
+                    window.vent.trigger('collection:loaded', theCollection);
+                }
+            });
+        },
+
+        singlePage: function (id) {
+            console.log(id);
+        }
+    });
+
+    App.kickstart = new App.Routes.Kickstart();
+    Backbone.history.start();
 
 }());
